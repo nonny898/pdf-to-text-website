450007a239275869d5b2612e5b794abb9f1a25a4
Sorawit Kongnurat <trainer.mapring@gmail.com>
2020-02-24 12:18:36 +0700

    recieve tar from binary

--- backend/controller/tar.js
+++ backend/controller/tar.js
@@ -1,3 +1,12 @@
-exports.uploadTar = (req, res, next) => {
+const tar = require('tar');
+const streamifier = require('streamifier');
+
+exports.postUploadTar = (req, res, next) => {
+  streamifier
+    .createReadStream(req.body)
+    .pipe(tar.t())
+    .on('entry', entry => {
+      console.log(entry);
+    });
   res.status(200).end();
 };
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 const tarController = require('../controller/tar');
 
-router.post('/uploadTar', tarController.uploadTar);
+router.post('/uploadTar', tarController.postUploadTar);
 
 module.exports = router;
