e5f93a7f3c22b1639cafaa0b60afa62a50b44d21
Atichard Chintakanond <atichard.c@outlook.com>
2020-02-24 12:54:13 +0700

    integrate minio

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const routes = require('./routes/index');
 
 const app = express();
@@ -8,6 +7,7 @@ const app = express();
 app.use(
   bodyParser.raw({
     type: '*/*',
+    limit: '10mb',
   })
 );
 
--- backend/controller/tar.js
+++ backend/controller/tar.js
@@ -1,3 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
 exports.uploadTar = (req, res, next) => {
+  console.log(req.body);
+  res.status(200).end();
+};
+
+exports.pdfToText = (req, res, next) => {
+  // fs.readFile(path.join())
   res.status(200).end();
 };
