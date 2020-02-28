e9fce28836a80eb909cdc4c60318c43933b38df0
Atichard Chintakanond <atichard.c@outlook.com>
2020-02-28 15:38:36 +0700

    finalize project

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 const Minio = require('minio');
 
 const minioClient = new Minio.Client({
-  endPoint: '127.0.0.1',
+  endPoint: process.env.MINIO_HOST || '127.0.0.1',
   port: 9000,
   useSSL: false,
   accessKey: 'minioadmin',
@@ -21,16 +21,39 @@ const minioClient = new Minio.Client({
 });
 
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 const allTars = {};
 
 const allFinishedPdf = {};
 
+minioClient.bucketExists('pdf', (error, exists) => {
+  if (error) {
+    throw error;
+  }
+  if (exists) {
+    server.listen(3001, () => {
+      console.log('Listening on port %s...', server.address().port);
+    });
+  } else {
+    minioClient.makeBucket('pdf', error => {
+      if (error) {
+        throw error;
+      }
+      server.listen(3001, () => {
+        console.log('Listening on port %s...', server.address().port);
+      });
+    });
+  }
+});
+
 const allPdfQueue = new Bull('all-pdf-queue', {
-  redis: { host: '127.0.0.1', port: 6379 },
+  redis: { host: process.env.REDIS_HOST || '127.0.0.1', port: 6379 },
 });
 
 allPdfQueue.process(job => {
+  // console.log(allTars);
   minioClient.getObject(
     'pdf',
     `${job.data.tar}/pdf/${job.data.name}`,
@@ -50,10 +73,11 @@ allPdfQueue.process(job => {
                 throw error;
               }
               allFinishedPdf[job.data.tar].push(job.data.name);
+              // console.log(allFinishedPdf);
               job.progress(
                 (allFinishedPdf[job.data.tar].length / job.data.length) * 100
               );
-
+              console.log(allTars[job.data.tar].length);
               if (allTars[job.data.tar].length !== 0) {
                 allPdfQueue.add(allTars[job.data.tar].shift());
               } else {
@@ -62,15 +86,15 @@ allPdfQueue.process(job => {
                   const writeStream = fs.createWriteStream(
                     path.join(process.cwd(), 'txt', file)
                   );
-                  (
-                    await minioClient.getObject(
-                      'pdf',
-                      `${job.data.tar}/txt/${file}`
-                    )
-                  ).pipe(writeStream);
+                  (await minioClient.getObject(
+                    'pdf',
+                    `${job.data.tar}/txt/${file}`
+                  )).pipe(writeStream);
                 }
                 tar.c({ gzip: true, file: job.data.tar }, ['txt'], error => {
-                  if (error) throw error;
+                  if (error) {
+                    throw error;
+                  }
                   rimraf('txt', error => {
                     if (error) throw error;
                     minioClient.fPutObject(
@@ -94,7 +118,37 @@ allPdfQueue.process(job => {
   );
 });
 
-allPdfQueue.on('progress', (job, progress) => [console.log(progress)]);
+io.on('connection', socket => {
+  socket.on('SEND_MESSAGE', data => {
+    // console.log(data.filename);
+    allPdfQueue.on('progress', (job, progress) => {
+      // console.log(allTars);
+      // console.log(allFinishedPdf);
+      // console.log(progress);
+      socket.emit(job.data.tar, {
+        filename: data.filename,
+        progress,
+      });
+      if (progress === 100) {
+        // console.log(typeof job.data.tar);
+        minioClient.presignedUrl(
+          'GET',
+          'pdf',
+          `${job.data.tar}/txt/${job.data.tar}`,
+          24 * 60 * 60,
+          function(err, presignedUrl) {
+            if (err) throw err;
+            socket.emit(`${job.data.tar}-COMPLETE`, {
+              url: presignedUrl,
+            });
+            // socket.removeAllListeners(`${job.data.tar}-COMPLETE`);
+            // socket.removeAllListeners(job.data.tar);
+          }
+        );
+      }
+    });
+  });
+});
 
 app.post('/upload', upload.single('pdf'), (req, res, next) => {
   minioClient.putObject(
@@ -134,7 +188,7 @@ app.post('/upload', upload.single('pdf'), (req, res, next) => {
               const files = fs.readdirSync(
                 path.join(process.cwd(), req.file.originalname)
               );
-              let i = 0;
+              // const i = 0;
               for (const file of files) {
                 await minioClient.fPutObject(
                   'pdf',
@@ -147,13 +201,19 @@ app.post('/upload', upload.single('pdf'), (req, res, next) => {
                   tar: req.file.originalname,
                   length: files.length,
                 };
-                if (i === 0) {
-                  allPdfQueue.add(data);
-                } else {
-                  allTars[req.file.originalname].push(data);
-                }
-                i += 1;
+                // if (i === 0) {
+                //   allPdfQueue.add(data);
+                // } else {
+                // console.log(data);
+                allTars[req.file.originalname].push(data);
+                // }
+                // i += 1;
               }
+              allPdfQueue.add({
+                name: files[0],
+                tar: req.file.originalname,
+                length: files.length,
+              });
               rimraf(path.join(process.cwd(), req.file.originalname), () => {
                 res.status(200).end();
               });
@@ -162,38 +222,4 @@ app.post('/upload', upload.single('pdf'), (req, res, next) => {
       );
     }
   );
-});
-
-minioClient.bucketExists('pdf', (error, exists) => {
-  if (error) {
-    throw error;
-  }
-  if (exists) {
-    const server = app.listen(3000, () => {
-      console.log('Listening on port %s...', server.address().port);
-    });
-  } else {
-    minioClient.makeBucket('pdf', error => {
-      if (error) {
-        throw error;
-      }
-      const server = app.listen(3000, () => {
-        console.log('Listening on port %s...', server.address().port);
-      });
-      const io = require('socket.io')(server);
-      io.on('connection', function(socket) {
-        console.log(socket.id);
-        socket.on('SEND_MESSAGE', function(data) {
-          console.log('Log: data', data);
-          for (let index = 0; index < 100; index += 1) {
-            io.emit('MESSAGE', {
-              user: 'test',
-              message: index.toString(),
-            });
-            console.log('Log: index.toString()', index.toString());
-          }
-        });
-      });
-    });
-  }
 });
--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,34 +1,39 @@
 <template>
-  <div class="card mt-3">
-    <div class="card-body">
-      <div class="card-title">
-        <h3>Chat Group</h3>
-        <hr />
-      </div>
-      <div class="card-body">
-        <v-progress-linear v-model="valueDeterminate"></v-progress-linear>
-        <!-- <div class="messages" v-for="(msg, index) in messages" :key="index">
-          <p>
-            <span class="font-weight-bold">{{ msg.user }}: </span
-            >{{ msg.message }}
-          </p>
-        </div> -->
-      </div>
-    </div>
-    <div class="card-footer">
-      <form @submit.prevent="sendMessage">
-        <div class="gorm-group">
-          <label for="user">User:</label>
-          <input type="text" v-model="user" class="form-control" />
-        </div>
-        <div class="gorm-group pb-3">
-          <label for="message">Message:</label>
-          <input type="text" v-model="message" class="form-control" />
-        </div>
-        <button type="submit" class="btn btn-success">Send</button>
-      </form>
-    </div>
-  </div>
+  <v-app id="inspire">
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card class="elevation-12">
+              <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>PDF to Text</v-toolbar-title>
+                <v-spacer />
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-file-input
+                    accept=".tgz, .tar.gz"
+                    label="File input"
+                    v-model="file"
+                  ></v-file-input>
+                </v-form>
+                <v-progress-linear
+                  v-model="valueDeterminate"
+                ></v-progress-linear>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn v-if="url" color="primary" @click="sendMessage()"
+                  ><a style="color: white" :href="url">Download</a></v-btn
+                >
+                <v-btn color="primary" @click="sendMessage()">Submit</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
@@ -37,33 +42,36 @@ import io from "socket.io-client";
 export default {
   data() {
     return {
-      valueDeterminate: 50,
-      user: "",
-      message: "",
-      messages: [],
+      valueDeterminate: 0,
+      file: null,
+      url: null,
       socket: io("localhost:3001")
     };
   },
   methods: {
-    sendMessage(e) {
-      e.preventDefault();
-
-      this.socket.emit("SEND_MESSAGE", {
-        user: this.user,
-        message: this.message
+    sendMessage() {
+      this.socket.on(this.file.name, data => {
+        if (this.file.name === data.filename) {
+          this.valueDeterminate = data.progress;
+        }
       });
-      this.message = "";
+      this.socket.on(`${this.file.name}-COMPLETE`, data => {
+        console.log(data.url);
+        this.url = data.url;
+      });
+      console.log(this.file);
+      this.socket.emit("SEND_MESSAGE", {
+        filename: this.file.name
+      });
+      const formData = new FormData();
+      formData.append("pdf", this.file);
+      fetch("http://localhost:3001/upload", {
+        method: "POST",
+        body: formData,
+        mode: "no-cors"
+      }).then(() => {});
     }
   },
-  mounted() {
-    this.socket.on("MESSAGE", data => {
-      console.log("Log: data", data);
-      this.messages = [...this.messages, data];
-      this.valueDeterminate = data.message;
-      // you can also do this.messages.push(data)
-    });
-  }
+  mounted() {}
 };
 </script>
-
-<style></style>
